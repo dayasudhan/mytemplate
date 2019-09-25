d9990e63780f993d9eb62a94f1e0d5c67447ee1a
Dayasudhan <dayasudhankg@gmail.com>
2019-09-25 20:28:39 +0530

    haage summane

--- app/src/main/java/com/kuruvatech/mytemplate/MainActivity.java
+++ app/src/main/java/com/kuruvatech/mytemplate/MainActivity.java
@@ -76,14 +76,6 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         setupViewPager(viewPager);
 //
